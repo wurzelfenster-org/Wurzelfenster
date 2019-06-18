544f894eb4ff8abfe0ec3bec312e8da6bd57324f
Jakub Czaplicki <jakub.czaplicki@tooploox.com>
2019-06-18 23:26:07 +0200

    Refactor the scan script: add options; check dependencies

--- WurzelScanner/Software/scan.sh
+++ WurzelScanner/Software/scan.sh
@@ -1,36 +1,110 @@
-#! /bin/bash
+#!/bin/bash
 
-start=`date +%s`
 
+print_usage() {
+    EXE_NAME="scan.sh"
+    echo "    "
+    echo "Usage:"
+    echo "    ./${EXE_NAME} "
+    echo "    "
+    echo "    Optional args:"
+    echo "    -d <dpi resolution> (default 600)"
+    echo "    -f <png|tiff|jpeg> scan output file format (default tiff)"
+    echo "    -l list connected scanners and exit"
+    echo "    -p <output file prefix> (default 'kresse')"
+    echo ""
+    echo "    example (simple should-just-work run): ./${EXE_NAME}"
+    echo "    example (list connected scanners): ./${EXE_NAME} -l"
+    echo "    example (quick scan test): ./${EXE_NAME} -d 50"
+}
+
+check_dependencies() {
+  hash scanimage 2>/dev/null || {
+    echo >&2 "Required 'scanimage' is not installed. Aborting.";
+    exit 1;
+    }
+
+  hash convert 2>/dev/null || {
+    echo >&2 "Required 'convert' is not installed. Aborting.";
+    exit 1;
+    }
+}
+
+
+# default values
+dpi=300
+scan_dir="/home/pi/scans/"
 counter_file="scanCount.txt"
-if [ -f $counter_file ]
+prefix="kresse"
+format=tiff
+
+
+# validate arguments
+while getopts "d:f:p:lh?" OPTION
+do
+    case ${OPTION} in
+        d)  if [[ $OPTARG =~ -.* ]]; then
+                echo "Missing argument for -d"
+                exit 1
+            fi
+            dpi=$OPTARG
+            ;;
+        f)  case ${OPTARG} in
+                "tiff") format=tiff;;
+                "png") format=png;;
+                "jpeg") format=jpeg;;
+                *) echo "Wrong argument for -f option!"; print_usage; exit 1;;
+            esac
+            ;;
+        l)  check_dependencies
+            scanimage -L
+            exit 0
+            ;;
+        p)  if [[ $OPTARG =~ -.* ]]; then
+                echo "Missing argument for -p"
+                exit 1
+            fi
+            prefix=$OPTARG
+            ;;
+        h)  print_usage
+            exit 0
+            ;;
+        ?)  print_usage
+            exit 1
+            ;;
+    esac
+done
+
+check_dependencies
+
+# run the script
+start=`date +%s`
+date=$(date -d "today" +"%Y-%m-%d-%H-%M")
+
+# get the scan number
+if [[ -f ${scan_dir}${counter_file} ]]
 then
-	count=`cat $counter_file`
+	count=`cat ${scan_dir}${counter_file}`
+    filename=${prefix}-${dpi}-$(printf %04d ${count})
+    ((count++))
+    echo $count > ${scan_dir}${counter_file}
 else
-    echo ERROR: the file $counter_file does not exist.
+    echo ERROR: the file ${scan_dir}${counter_file} does not exist.
 fi
 
-scanDir="/media/card/"
-prefix="kresse"
-dpi="600"
-date=$(date -d "today" +"%Y-%m-%d-%H-%M")
-filename=$prefix-$dpi-$(printf %04d $count)
-
-((count++))
-echo $count > $counter_file
-
+# scan
 startScan=`date +%s`
-scanimage --mode=color --resolution=$dpi --format=tiff -l0 -t0 -x215 -y297 > $filename.tiff
+scanimage --mode=color --resolution=$dpi --format=$format -l0 -t0 -x215 -y297 > ${scan_dir}${filename}.${format}
 endScan=`date +%s`
 runtimeScan=$((endScan-startScan))
 echo "scan done. $runtimeScan sec"
 
+# convert tiff to png
 startConversion=`date +%s`
-convert $filename.tiff $scanDir$filename.png
+convert ${scan_dir}${filename}.${format} ${scan_dir}${filename}.png
 endConversion=`date +%s`
 runtimeConversion=$((endConversion-startConversion))
 echo "conversion done. $runtimeConversion sec"
-
 rm $filename.tiff
 echo "cleanup done"
 
